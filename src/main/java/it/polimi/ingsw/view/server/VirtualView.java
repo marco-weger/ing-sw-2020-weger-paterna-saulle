7ce15e0dd426a24eb0e08da8ff82a44a4e046c07
Francesco Paterna <francesco.paterna@mail.polimi.it>
2020-04-01 17:36:40 +0200

    added playersName to VirtualView

--- src/main/java/it/polimi/ingsw/view/server/VirtualView.java
+++ src/main/java/it/polimi/ingsw/view/server/VirtualView.java
@@ -2,8 +2,13 @@ package it.polimi.ingsw.view.server;
 
 import it.polimi.ingsw.Observable;
 import it.polimi.ingsw.Observer;
+import it.polimi.ingsw.model.Player;
+
+import java.util.ArrayList;
 
 public class VirtualView extends Observable implements Observer {
+
+    private ArrayList<Player> playersName;
 
     @Override
     public void notifyObserver(Object arg) {}
