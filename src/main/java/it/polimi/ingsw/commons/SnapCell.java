60ae37527b315e1d0400c3bdd92b3d1f099908ba
Francesco Paterna <francesco.paterna@mail.polimi.it>
2020-04-06 04:04:02 +0200

    javadoc client message

--- src/main/java/it/polimi/ingsw/commons/SnapCell.java
+++ src/main/java/it/polimi/ingsw/commons/SnapCell.java
@@ -6,6 +6,14 @@ public class SnapCell implements Serializable {
 
     public int row, column, level;
 
+    /**
+     *
+     * @param row thw row of the cell
+     * @param column the coloumn of the cell
+     * @param level the level of the cell
+     *a "snap-class" to guarantee a safetly trasmission of model.cell class information to the view
+     *through the controller. (MVC pattern)
+     */
     public SnapCell(int row, int column, int level){
         this.row=row;
         this.column=column;
--- src/main/java/it/polimi/ingsw/commons/SnapWorker.java
+++ src/main/java/it/polimi/ingsw/commons/SnapWorker.java
@@ -7,6 +7,15 @@ public class SnapWorker implements Serializable {
     public String name;
     public int row, column, n;
 
+    /**
+     *
+     * @param row row of the worker
+     * @param column column of the worker
+     * @param name name of the player
+     * @param n number of the worker
+     * a "snap-class" to guarantee a safetly trasmission of model.worker information to the view
+     * through the controller. (MVC pattern)
+     */
     public SnapWorker(int row, int column, String name, int n){
         this.name=name;
         this.row=row;
