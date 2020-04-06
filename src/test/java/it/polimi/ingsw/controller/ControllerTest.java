8b749801e4622945130fdfb14fc870c3f646e521
Francesco Paterna <francesco.paterna@mail.polimi.it>
2020-04-06 15:47:32 +0200

    added controllerTest

---
+++ src/test/java/it/polimi/ingsw/controller/ControllerTest.java
@@ -0,0 +1,68 @@
+package it.polimi.ingsw.controller;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class ControllerTest {
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void handleMessage() {
+    }
+
+    @Test
+    void testHandleMessage() {
+    }
+
+    @Test
+    void testHandleMessage1() {
+    }
+
+    @Test
+    void testHandleMessage2() {
+    }
+
+    @Test
+    void testHandleMessage3() {
+    }
+
+    @Test
+    void testHandleMessage4() {
+    }
+
+    @Test
+    void testHandleMessage5() {
+    }
+
+    @Test
+    void testHandleMessage6() {
+    }
+
+    @Test
+    void testHandleMessage7() {
+    }
+
+    @Test
+    void testHandleMessage8() {
+    }
+
+    @Test
+    void testHandleMessage9() {
+    }
+
+    @Test
+    void endGame() {
+    }
+
+    @Test
+    void inizializeMatch() {
+    }
+
+    @Test
+    void startTurn() {
+    }
+}
