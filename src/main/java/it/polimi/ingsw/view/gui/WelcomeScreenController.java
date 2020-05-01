ad1ac1461bb4985a1019aea449c235a7ecbba505
Francesco Paterna <francesco.paterna@mail.polimi.it>
2020-05-01 17:34:50 +0200

    Rules Added

--- src/main/java/it/polimi/ingsw/view/gui/WelcomeScreenController.java
+++ src/main/java/it/polimi/ingsw/view/gui/WelcomeScreenController.java
@@ -1,12 +1,33 @@
 package it.polimi.ingsw.view.gui;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 
 public class WelcomeScreenController {
     public Button PlayButton;
     public Button HelpButton;
+
+
+    public void HandleHelpButton(javafx.event.ActionEvent actionEvent) {
+        Parent root;
+    try{
+        root = FXMLLoader.load(getClass().getResource("/it.polimi.ingsw/view/gui/FXMLfiles/Rules.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Santorini_Rules_eng");
+        stage.setScene(new Scene(root, 450, 450));
+        stage.show();
+    }
+    catch (IOException e) {
+            e.printStackTrace();
+
+    }
+    }
 }
