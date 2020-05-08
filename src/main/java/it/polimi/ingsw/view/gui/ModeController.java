762826006c129fa2d7d182aea3295b029127f5fb
marco-weger <weger98@me.com>
2020-05-09 01:53:06 +0200

    GUI

--- src/main/java/it/polimi/ingsw/view/gui/ModeController.java
+++ src/main/java/it/polimi/ingsw/view/gui/ModeController.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.view.gui;
 
+import it.polimi.ingsw.commons.clientmessages.ModeChoseClient;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
