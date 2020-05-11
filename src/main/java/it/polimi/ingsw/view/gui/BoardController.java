29ea5273d84d62f4ec4bee0eb7569de02cfecc63
Francesco Paterna <francesco.paterna@mail.polimi.it>
2020-05-11 03:44:09 +0200

    Board Loaded

--- src/main/java/it/polimi/ingsw/view/gui/BoardController.java
+++ src/main/java/it/polimi/ingsw/view/gui/BoardController.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.view.gui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 
@@ -13,5 +14,81 @@ public class BoardController extends DefaultController {
         super.setBackground(new Image("/it.polimi.ingsw/view/gui/img/scene/bg_match.png"));
     }
 
+
+    public void cell00(ActionEvent actionEvent) {
+    }
+
+    public void cell01(ActionEvent actionEvent) {
+    }
+
+    public void cell02(ActionEvent actionEvent) {
+    }
+
+    public void cell03(ActionEvent actionEvent) {
+    }
+
+    public void cell04(ActionEvent actionEvent) {
+    }
+
+    public void cell10(ActionEvent actionEvent) {
+    }
+
+    public void cell11(ActionEvent actionEvent) {
+    }
+
+    public void cell12(ActionEvent actionEvent) {
+    }
+
+    public void cell13(ActionEvent actionEvent) {
+    }
+
+    public void cell14(ActionEvent actionEvent) {
+    }
+
+    public void cell20(ActionEvent actionEvent) {
+    }
+
+    public void cell21(ActionEvent actionEvent) {
+    }
+
+    public void cell22(ActionEvent actionEvent) {
+    }
+
+    public void cell23(ActionEvent actionEvent) {
+    }
+
+    public void cell24(ActionEvent actionEvent) {
+    }
+
+    public void cell30(ActionEvent actionEvent) {
+    }
+
+    public void cell31(ActionEvent actionEvent) {
+    }
+
+    public void cell32(ActionEvent actionEvent) {
+    }
+
+    public void cell33(ActionEvent actionEvent) {
+    }
+
+    public void cell34(ActionEvent actionEvent) {
+    }
+
+    public void cell40(ActionEvent actionEvent) {
+    }
+
+    public void cell41(ActionEvent actionEvent) {
+    }
+
+    public void cell42(ActionEvent actionEvent) {
+    }
+
+
+    public void cell43(ActionEvent actionEvent) {
+    }
+
+    public void cell44(ActionEvent actionEvent) {
+    }
 
 }
