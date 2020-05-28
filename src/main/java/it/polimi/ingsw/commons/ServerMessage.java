5d877a3ee7367c16846c64728cf4771862c1c36f
marco-weger <weger98@me.com>
2020-05-28 14:21:49 +0200

    Add easter egg

--- src/main/java/it/polimi/ingsw/commons/ServerMessage.java
+++ src/main/java/it/polimi/ingsw/commons/ServerMessage.java
@@ -5,6 +5,9 @@ import it.polimi.ingsw.view.ViewInterface;
 import java.io.Serializable;
 
 public abstract class ServerMessage implements Serializable {
+
+   private static final long serialVersionUID = 86786458382L;
+
    public abstract void accept(ViewInterface vi);
    public ServerMessage(){}
 }
